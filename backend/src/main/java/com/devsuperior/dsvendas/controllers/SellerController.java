7823863879a60b0ee99bfda7945bcb2bc755b173
Nilvan Calista <nil.calista@gmail.com>
2021-09-11 23:14:36 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SellerController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SellerController.java
@@ -12,7 +12,7 @@ import com.devsuperior.dsvendas.dto.SellerDTO;
 import com.devsuperior.dsvendas.services.SellerService;
 
 @RestController
-@RequestMapping(value = "/Sellers")
+@RequestMapping(value = "/sellers")
 public class SellerController {
 
 	@Autowired
